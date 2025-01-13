e53cf9576df850371b538b7f95efb286b1f54a86
siljemogster <125352923+siljemogster@users.noreply.github.com>
2025-01-13 20:48:12 +0100

    register handler js

--- js/events/auth/registerHandler.js
+++ js/events/auth/registerHandler.js
@@ -1,17 +1,31 @@
-export function registerHandler () {
-    console.log("registerHandler"); 
-    const form = document.querySelector("#registerForm"); 
+export function registerHandler() {
+  console.log("registerHandler");
+  const form = document.querySelector("#registerForm");
 
-    if(form) {
-        form.addEventListener("submit", submitForm);
-    }
+  if (form) {
+    form.addEventListener("submit", submitForm);
+  }
 }
 
 function submitForm(event) {
-    event.preventDefault();
-    const form = event.target; 
-    const formData = new FormData(form); 
-    const data = Object.fromEntries(formData);
-    console.log(data);
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
 
+  if (data.bio.trim() === "") {
+    delete data.bio;
+  }
+
+  if (data.avatarUrl.treim() === "") {
+    delete data.avatarUrl;
+  } else {
+    data.avatar = {
+      url: data.avatarUrl,
+      alt: `${data.name} 's avatar`,
+    };
+    delete data.avatarUrl;
+  }
+
+  console.log(data);
 }
